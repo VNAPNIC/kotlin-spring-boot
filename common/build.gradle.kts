b1ed9a76edd769c45ea2250c2ee04ee39c006202
vnapnic <vn.apnic@gmail.com>
2021-05-17 16:14:44 +0700

    update gradle

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -3,6 +3,12 @@ dependencies {
 
     implementation("com.google.code.gson:gson:2.7")
     implementation("io.jsonwebtoken:jjwt:0.9.1")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    runtimeOnly("org.springframework.boot:spring-boot-devtools")
+
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.data:spring-data-commons")
     implementation("org.springframework.boot:spring-boot-starter-validation")
