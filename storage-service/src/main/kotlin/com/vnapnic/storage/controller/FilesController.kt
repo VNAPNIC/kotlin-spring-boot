951f1972ec9e332fa45ca1b1f017a75dcc4f51fb
vnapnic <vn.apnic@gmail.com>
2021-05-24 00:01:18 +0700

    create FilesController.kt

--- storage-service/src/main/kotlin/com/vnapnic/storage/controller/FilesController.kt
+++ storage-service/src/main/kotlin/com/vnapnic/storage/controller/FilesController.kt
@@ -2,6 +2,8 @@ package com.vnapnic.storage.controller
 
 import com.vnapnic.storage.services.FilesService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.io.Resource
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -12,9 +14,20 @@ class FilesController {
     @Autowired
     lateinit var filesService: FilesService
 
+    /**
+     * https://github.com/linlinjava/litemall/blob/master/litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxStorageController.java#L53
+     */
     @GetMapping("/fetch/{key:.+}")
-    fun fetch(@PathVariable key: String) {
+    fun fetch(@PathVariable key: String?) : ResponseEntity<Resource>{
 
+        if (key == null) {
+            return ResponseEntity.notFound().build();
+        }
+        if (key.contains("../")) {
+            return ResponseEntity.badRequest().build();
+        }
+//        return ResponseEntity.ok().contentType(mediaType).body(file)
+        return  null
     }
 
     @GetMapping("/download/{key:.+}")
