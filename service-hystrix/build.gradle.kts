780c9f33ac310f058395fe566d148963ac8fbb96
vnapnic <vn.apnic@gmail.com>
2021-06-04 16:46:44 +0700

    fix eureka

--- service-hystrix/build.gradle.kts
+++ service-hystrix/build.gradle.kts
@@ -2,6 +2,4 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-hystrix")
     implementation("org.springframework.cloud:spring-cloud-netflix-hystrix-dashboard")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.cloud:spring-cloud-config-client")
-    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 }
